1471db01a5ea6c9fde26320b649130433bb62339
James Sullivan <jsullivan@cambridgecloudpartners.com>
2014-06-05 12:09:49 -0400

    looking for the freshbooks post data

--- webhooks/freshbooks.py
+++ webhooks/freshbooks.py
@@ -5,5 +5,7 @@ app = Blueprint('freshbooks_webhook', __name__)
 @app.route("/webhooks/freshbooks", methods=['POST'])
 def freshbooks_webhook():
     # just needed during setup
+    print request
     print request.data
+    print request.form
     return "ok"
